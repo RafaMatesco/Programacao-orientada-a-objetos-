25d6068cddf673ea5b47fe977191d09e480c93cb
Rafael Matesco <matescorapha@gmail.com>
2023-03-19 19:54:33 -0300

    Projeto finalizado

--- Projeto bimestre 1/APP1.java
+++ Projeto bimestre 1/APP1.java
@@ -1,4 +1,16 @@
-class DETRAN{
+/* *******************************************************************
+ * Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ * Curso Técnico em Informática - Data de Entrega: 20/03/2023
+ * Autores do Projeto: Rafael Giordano
+ *                     Paulo Renato
+ * Turma: 3X  Disciplina: Programação - II
+ * Avaliação parcial referente ao 1 - Bimestre
+ * Observação: <colocar se houver>
+ * 
+ * APP1.java
+ * ******************************************************************/
+
+class App1{
         
         public static void main(String args[])
         {
@@ -15,7 +27,6 @@ class DETRAN{
 
 
                 float porcentagemCasos = 0;
-                float mediaCasosMulher = 0;
 
 
                for( int x = 0; x <= args.length-1; x+=3)
@@ -31,30 +42,87 @@ class DETRAN{
                         if(procedencia==1)casosInterior++;
                         if(2023-anoNasc>60)casosMaior60++;
 
-                        System.out.printf("Ano de nascimento: %d, Sexo: %d, Procedencia: %d \n",anoNasc, sexo, procedencia);
+                        System.out.printf("----\nAno de nascimento: %d, Sexo: %d, Procedencia: %d \n",anoNasc, sexo, procedencia);
                }
         
 
                 porcentagemCasos = (CasosMenor21/totalCasos)*100;
                 
 
-                System.out.printf("Total de casos = %d\n",totalCasos);
-                System.out.printf("Porcentagem de casos menores de 21 anos = %1.2f%%\n",porcentagemCasos);
-                System.out.printf("Casos com mulheres = %.0f\n",casosMulher);
-                System.out.printf("Casos no interior = %d\n",casosInterior);
+                System.out.printf("Total de casos = %d\n----\n",totalCasos);
+                System.out.printf("Porcentagem de casos menores de 21 anos = %1.2f%%\n----\n",porcentagemCasos);
+                System.out.printf("Casos com mulheres = %.0f\n----\n",casosMulher);
+                System.out.printf("Casos no interior = %d\n----\n",casosInterior);
                 casosMulher = (casosMulher/totalCasos);
                 casosHomem = (casosHomem/totalCasos);
                 if(casosMaior60>0){
-                        System.out.printf("Existe(m) %d caso(s) com maiores de 60 anos\n",casosMaior60);
+                        System.out.printf("Existe(m) %d caso(s) com maiores de 60 anos\n----\n",casosMaior60);
                 }else{
-                        System.out.printf("Nao existem casos com maiores de 60 anos\n");
+                        System.out.printf("Nao existem casos com maiores de 60 anos\n----\n");
                 
 
                 }
-                System.out.printf("Media de casos com mulheres = %1.0f\n",casosMulher);
-                System.out.printf("Media de casos com homens = %1.0f\n",casosHomem);
-                System.out.printf("Porcentagem de casos com mulheres = %1.2f%%\n",casosMulher*100);
-                System.out.printf("Porcentagem de casos com homens = %1.2f%%\n",casosHomem*100);
+                System.out.printf("Media de casos com mulheres = %1.0f\n----\n",casosMulher);
+                System.out.printf("Media de casos com homens = %1.0f\n----\n",casosHomem);
+                System.out.printf("Porcentagem de casos com mulheres = %1.2f%%\n----\n",casosMulher*100);
+                System.out.printf("Porcentagem de casos com homens = %1.2f%%\n----\n",casosHomem*100);
 
         }       
 }
+
+
+
+
+ class App2{
+    
+        public static void main(String args[]){
+            //args[0] = quantidade de dias 
+            //args[1,3,5,...] = quantidade de grupos
+            //args[2,4,6,...] = quantidade de alunos
+            
+            
+            // variaveis
+            int numeroGrupos = 0;
+            int numeroAlunos = 0;
+            int menorGrupo = 0;
+            int dia = 1;
+            
+                    
+            //testa de a quantidade de dados por dia esta correta
+            if(Integer.parseInt(args[0]) == (args.length - 1)/2){
+                //para cada dia
+                for (int i=1; i<args.length; i+=2){
+    
+                    numeroGrupos = Integer.parseInt(args[i]);
+                    numeroAlunos = Integer.parseInt(args[i+1]);
+    
+                    //imprime o dia 
+                    System.out.printf("Dia %d \n",dia);
+                    dia++;
+    
+                    //testa se o numero de alunos é par
+                    if(numeroAlunos%2==1){
+                        System.out.printf("numero de alunos deve ser par");
+                    }else{
+    
+                        //calcula o menor grupo
+                        menorGrupo = ((numeroAlunos/numeroGrupos)-numeroGrupos)+1;
+                        
+                        //testa se a compatibilidade de alunos e grupos existe
+                        if(menorGrupo%2==1){
+                            System.out.printf("Nao existe numeros pares consecutivos que formen %d grupos de %d alunos",
+                            numeroGrupos,numeroAlunos);
+                        }else{
+                            //imprime os grupos
+                            for(int i2=0;i2<numeroGrupos;i2++){
+                                System.out.printf("%d ",menorGrupo+(i2*2));
+                            }   
+                        }
+                    }
+                    System.out.println("\n");
+                }
+            }else{
+                System.out.printf("!!!inconsistencia de dados por dia");
+            }
+        }
+    }
